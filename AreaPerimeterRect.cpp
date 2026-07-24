c33699e0ef7212383779f5754e2c5140c862a7c3
dhanrajjadhav0102-dotcom <dhanraj.jadhav0102@gmail.com>
2026-07-24 04:54:22 +0000

    changes

---
+++ AreaPerimeterRect.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int length, width;
+    cout << "Enter length: ";
+    cin >> length;
+    cout << "Enter width: ";
+    cin >> width;
+    int area = length * width;
+    int perimeter = 2 * (length + width);
+    cout << "Area = " << area << endl;
+    cout << "Perimeter = " << perimeter << endl;
+    return 0;
+}
