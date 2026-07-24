28f5468d8b175eaa80d4ff783629a56373618765
dhanrajjadhav0102-dotcom <dhanraj.jadhav0102@gmail.com>
2026-07-24 04:27:45 +0000

    changes

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Enter first value: ";
+    cin >> a;
+    cout << "Enter second value: ";
+    cin >> b;
+    cout << "Sum = " << a + b << endl;
+    return 0;
+}
