a573ff7ea269bb763afd978d9088f80d5bf80b99
dhanrajjadhav0102-dotcom <dhanraj.jadhav0102@gmail.com>
2026-07-31 09:39:16 +0530

    Add files via upload

---
+++ natural.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, sum = 0;
+    cin >> n;
+    while (n > 0) {
+        sum += n;
+        n--;
+    }
+    cout << sum;
+    return 0;
+}
