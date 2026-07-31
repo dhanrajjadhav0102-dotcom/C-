a66e206119ba1d56d9336ec6627d995d1d2a1cc2
dhanrajjadhav0102-dotcom <dhanraj.jadhav0102@gmail.com>
2026-07-31 04:36:56 +0000

    changes

---
+++ naturalsum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, sum = 0;
+   for(a=1;a<=5;a++){
+   cout<<"n natural numbers "<<a<<endl;
+   sum=sum+a;}
+cout<<"sum of natural numbers"<<sum<<endl;
+return 0;
+}
