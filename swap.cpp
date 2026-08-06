b86383e9cfddae1b3ac4ad5e11826819b4e0547b
dhanrajjadhav0102-dotcom <dhanraj.jadhav0102@gmail.com>
2026-08-06 18:40:15 +0000

    changes

---
+++ swap.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     int a, b, temp;
+
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "\nBefore swapping:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    temp = a; 
+    a = b;   
+    b = temp; 
+
+    cout << "\nAfter swapping:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+}
